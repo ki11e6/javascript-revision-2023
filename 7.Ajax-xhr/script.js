eda981f7af216ca50afa34529d1e293e616b1e57
sharath <sharath.surendran.m@gmail.com>
2023-07-03 19:50:44 +0530

    ajax-xhr

---
+++ 7.Ajax-xhr/script.js
@@ -0,0 +1,34 @@
+const xhr = new XMLHttpRequest();
+
+//! Sepcify method and endpoint/URL
+
+// xhr.open('GET', './movies.json');
+xhr.open('GET', 'https://api.github.com/users/ki11e6/repos');
+
+//! readyState has 5 possible values
+//* - 0: request not initialized
+//* - 1: server connection established
+//* - 2: request received
+//* - 3: processing request
+//* - 4: request finished and response is ready
+
+xhr.onreadystatechange = function () {
+    // console.log(this.readyState);
+    if (this.readyState === 4 && this.status === 200) {
+        // console.log(JSON.parse(this.responseText));
+        const data = JSON.parse(this.responseText);
+        // data.forEach((movie) => {
+        //     const li = document.createElement('li');
+        //     li.innerHTML = `<strong>${movie.title}</strong>-${movie.year}`;
+        //     document.querySelector('#results').appendChild(li);
+        // });
+        data.forEach((repo) => {
+            // console.log(repo);
+            const li = document.createElement('li');
+            li.innerHTML = `<strong>${repo.name}</strong>-${repo.git_url}`;
+            document.querySelector('#results').appendChild(li);
+        });
+    }
+};
+
+xhr.send();
